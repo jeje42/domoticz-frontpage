bb395964b3cdc20069d33c37b2a2288ba09909dd
jeje <guidon@live.fr>
2019-02-09 18:55:41 +0100

    test change awesome icon

--- custom/js/dashboard.js
+++ custom/js/dashboard.js
@@ -47,11 +47,42 @@ Domotica = {
 
 			},
 			switches: function() {
+				this.switchesJejeServeur()
+
 				$( ".switch.default-ui").each(function(idx,el) {
 					// Add icon
 					$(el).prepend(
 						$("<span>" ).addClass( "info-box-icon" )
 							.append( $( "<i>" ).addClass( "fa fa-lightbulb-o") )
+					);
+
+					// Add content itself. Title is given in an element with class title, or in a data-attribute
+					var title = $(el).find( ".title" );
+
+					if( title.length == 0 ) {
+						title = $( "<span>" ).text( $(el).data( "title" ) );
+					}
+
+					var content = $( "<div>" )
+						.addClass( "info-box-content" )
+						.append( title.addClass( "info-box-text" ) )
+						.append( $( "<span>" ).addClass( "info-box-number domoticz-status" ) )
+						.append( $( "<div>" ).addClass( "progress" ) )
+						.append( $( "<span>" ).addClass( "progress-description lastUpdate" ) )
+
+					$(el).append(content);
+
+					// Add overlay
+					Domotica.dashboard.defaultUI.loadingOverlay($(el));
+				});
+			},
+
+			switchesJejeServeur: function() {
+				$( ".switch.serveur-ui").each(function(idx,el) {
+					// Add icon
+					$(el).prepend(
+						$("<span>" ).addClass( "info-box-icon" )
+							.append( $( "<i>" ).addClass( "fas fa-server") )
 					);
 
 					// Add content itself. Title is given in an element with class title, or in a data-attribute
